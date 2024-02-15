937b8f9b3140fcea508d924b07aaf51b02f6b2ee
Zixi Liu <zixliu@zixi.inf.ethz.ch>
2024-02-15 11:21:28 +0100

    seems to be finished

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,27 @@ extern crate rustc_errors;
 extern crate rustc_hash;
 extern crate rustc_hir;
 extern crate rustc_interface;
+extern crate rustc_middle;
 extern crate rustc_session;
 extern crate rustc_span;
-extern crate rustc_middle;
 
-use std::{any::Any, path, process, str::{self, FromStr}, sync::Arc};
-use rustc_middle::{hir::nested_filter, query::Key, ty::{Ty, TyCtxt, TyKind}};
-use rustc_ast_pretty::pprust::item_to_string;
+
 use rustc_errors::registry;
-use rustc_session::config;
 use rustc_errors::DIAGNOSTICS;
+use rustc_hir::Expr;
+use rustc_middle::ty::TyCtxt;
+use rustc_session::config;
 use std::path::PathBuf;
-use rustc_hir::{intravisit::Visitor, Stmt};
-use rustc_span::source_map::SourceMap;
-use rustc_hir::{Expr, Item, def::Res};
+use std::{
+    path, process,
+    str::{self, FromStr},
+    sync::Arc,
+};
+use serde::{Serialize, Deserialize};
+use serde_json;
+use std::fs::File;
+use std::io::Write;
+use serde_json::Value;
 
 
 pub fn get_config(input_path: PathBuf) -> rustc_interface::Config {
@@ -56,8 +63,9 @@ pub fn get_config(input_path: PathBuf) -> rustc_interface::Config {
     config
 }
 
-#[derive(Debug)]
-pub struct VarInfo<'tcx> {
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct VarInfo {
     name: String,
     start_line: usize,
     start_col: usize,
@@ -65,7 +73,7 @@ pub struct VarInfo<'tcx> {
     end_line: usize,
     end_col: usize,
     end_file: Option<PathBuf>,
-    ty: Option<Ty<'tcx>>,
+    ty: Option<String>,
 }
 
 fn extract_local_path(name: &rustc_span::FileName) -> Option<PathBuf> {
@@ -80,10 +88,9 @@ fn extract_local_path(name: &rustc_span::FileName) -> Option<PathBuf> {
     }
 }
 
-
-
 struct HirVisitor<'tcx> {
     tcx: TyCtxt<'tcx>,
+    info: Vec<String>,
 }
 
 struct All;
@@ -94,7 +101,6 @@ impl<'hir> rustc_hir::intravisit::nested_filter::NestedFilter<'hir> for All {
     const INTRA: bool = true;
 }
 
-
 impl<'tcx> rustc_hir::intravisit::Visitor<'tcx> for HirVisitor<'tcx> {
     type Map = rustc_middle::hir::map::Map<'tcx>;
     type NestedFilter = All;
@@ -103,7 +109,7 @@ impl<'tcx> rustc_hir::intravisit::Visitor<'tcx> for HirVisitor<'tcx> {
         self.tcx.hir()
     }
 
-
+    // Represents a `let` statement (i.e., `let <pat>:<ty> = <init>;`).
     fn visit_local(&mut self, local: &'tcx rustc_hir::Local<'tcx>) {
         let source_map = self.tcx.sess.source_map();
         let ident_name = local.pat.simple_ident().unwrap().name.as_str().to_string();
@@ -120,7 +126,7 @@ impl<'tcx> rustc_hir::intravisit::Visitor<'tcx> for HirVisitor<'tcx> {
             let hir_id = expr.hir_id;
             let def_id = hir_id.owner.def_id;
             let ty = self.tcx.typeck(def_id).node_type(hir_id);
-            Some(ty)
+            Some(ty.to_string())
         } else {
             None
         };
@@ -135,30 +141,36 @@ impl<'tcx> rustc_hir::intravisit::Visitor<'tcx> for HirVisitor<'tcx> {
             start_file: start_path,
             end_file: end_path,
         };
-        println!("{:#?}", var_info);
+        let var_info_json = serde_json::to_string(&var_info).unwrap();
+        self.info.push(var_info_json);
+        //println!("{:#?}", var_info_json);
+
         rustc_hir::intravisit::walk_local(self, local);
     }
 
-    fn visit_expr(&mut self, expr: &'tcx Expr<'tcx>) {
+    // AssignExpr: An assignment (e.g., `a = foo()`).
+    // We only consider the right hand of the assignment expr
+    fn visit_expr(&mut self, ex: &'tcx Expr<'tcx>) {
         let source_map = self.tcx.sess.source_map();
 
-        if let rustc_hir::ExprKind::Path(qpath) = expr.kind {
-            if let rustc_hir::QPath::Resolved(_, p) = qpath {
+        if let rustc_hir::ExprKind::Assign(rh_ex, _, _) = ex.kind {
+            let hir_id = rh_ex.hir_id;
+            let def_id = hir_id.owner.def_id;
+            let ty = self.tcx.typeck(def_id).node_type(hir_id).to_string();
+            if let rustc_hir::ExprKind::Path(qpath) = rh_ex.kind {
+                if let rustc_hir::QPath::Resolved(_, p) = qpath {
+                    if let Some(seg) = p.segments.last() {
+                        //TODO: Didn't consider the more than 1 pathseg: `mod_a::StructB`
+                        let var_name = seg.ident.name.as_str().to_string();
+                        let var_span = p.span.data();
 
-                for seg in p.segments.into_iter() {
-                    if let rustc_hir::def::Res::Local(hir_id) = p.res {
-                        let def_id = hir_id.owner.def_id;
-                        let ty = self.tcx.typeck(def_id).node_type(hir_id);
-                        let ident_name = seg.ident.name.as_str().to_string();
-                        let var_span = seg.ident.span.data();
                         let start = source_map.lookup_char_pos(var_span.lo);
                         let end = source_map.lookup_char_pos(var_span.hi);
-
                         let start_path = extract_local_path(&start.file.name);
                         let end_path = extract_local_path(&end.file.name);
 
                         let var_info = VarInfo {
-                            name: ident_name,
+                            name: var_name,
                             start_line: start.line,
                             start_col: start.col_display,
                             end_line: end.line,
@@ -167,15 +179,17 @@ impl<'tcx> rustc_hir::intravisit::Visitor<'tcx> for HirVisitor<'tcx> {
                             start_file: start_path,
                             end_file: end_path,
                         };
-                        println!("{:#?}", var_info);
+                        let var_info_json = serde_json::to_string(&var_info).unwrap();
+                        self.info.push(var_info_json);
+                        //println!("{:#?}", var_info_json);
                     }
                 }
-
             }
         }
-        //println!("{:#?}", expr);
-        rustc_hir::intravisit::walk_expr(self, expr);
+        rustc_hir::intravisit::walk_expr(self, ex);
     }
+
+
 }
 
 fn main() {
@@ -190,19 +204,43 @@ fn main() {
                 //let source_map = tcx.sess.source_map();
                 //parse(tcx);
 
-                let mut visitor = HirVisitor {
+                let mut visitor = HirVisitor { 
                     tcx,
+                    info: Vec::new(),
                 };
 
-                
                 for id in hir_krate.items() {
-                    // let item = hir_krate.item(id);
                     rustc_hir::intravisit::Visitor::visit_nested_item(&mut visitor, id);
-
-
                 }
-                
+
+                //println!("{:#?}", visitor.info);
+
+                // save to JSON file
+                let file_path = "output.json";
+                if let Err(e) = save_json_array_to_file(visitor.info, file_path) {
+                    println!("An error occurred: {}", e);
+                } else {
+                    println!("JSON array has been saved to {} successfully.", file_path);
+                }
             })
         });
     });
+}
+
+
+fn save_json_array_to_file(json_strings: Vec<String>, file_path: &str) -> std::io::Result<()> {
+    // 将所有的JSON字符串转换为serde_json::Value，并收集到一个Vec中
+    let json_values: Vec<Value> = json_strings
+        .into_iter()
+        .filter_map(|s| serde_json::from_str(&s).ok())
+        .collect();
+
+    // 将Vec<Value>转换为一个JSON数组字符串
+    let json_array = serde_json::to_string(&json_values).unwrap_or_else(|_| "[]".to_string());
+
+    // 创建并写入文件
+    let mut file = File::create(file_path)?;
+    file.write_all(json_array.as_bytes())?;
+
+    Ok(())
 }
--- src/t.rs
+++ src/t.rs
@@ -1,5 +1,22 @@
+// 定义模块mod_a
+mod mod_a {
+    // 在mod_a内部定义结构体StructB
+    pub struct StructB {
+        // StructB包含一个公共字段field_c
+        pub field_c: i32,
+    }
+
+    fn foo() {
+        let a=10;
+        let mut b:bool = false;
+    }
+}
+
 fn main() {
-    let number: f64 = 1.0;
-    let width: usize = 5;
-    println!("{number:>width$}");
+    // 创建StructB的实例，并初始化field_c
+    let mut instance_b = mod_a::StructB { field_c: 10 };
+    instance_b = mod_a::StructB { field_c: 11 };
+
+    // 访问field_c字段
+    println!("field_c value: {}", instance_b.field_c);
 }
